3d39c070556103d77a0443709d760f8c1a6142bc
Matt Basile <matthewbasile2@gmail.com>
2019-03-05 16:34:55 -0500

    UI updating according to strikes balls and outs

--- baseball/src/Count.js
+++ baseball/src/Count.js
@@ -3,9 +3,35 @@ import React from 'react'
 export default function Count(props) {
   return (
     <div className="w-1/4 text-center self-center">
-      <h3 className="my-3 text-2xl">Strikes: {props.strikes}</h3>
-      <h3 className="my-3 text-2xl">Balls: {props.balls}</h3>
-      <h3 className="my-3 text-2xl">Outs: {props.outs}</h3>
+    <div className="flex items-baseline my-3">
+      <h3 className="text-2xl">Strikes: </h3>
+      {props.strikes === 0 ? null : 
+      props.strikes === 1 ? <div className="bg-green-light rounded h-4 w-4 mx-2"></div> :
+     (<><div className="bg-green-light rounded h-4 w-4 mx-2"></div>
+      <div className="bg-green-light rounded h-4 w-4 mx-2"></div></>)
+      }
+    </div>
+    <div className="flex items-baseline my-3">
+      <h3 className="text-2xl">Balls: </h3>
+      {props.balls === 0 ? null : 
+      props.balls === 1 ? <div className="bg-orange-light rounded h-4 w-4 mx-2"></div> :
+      props.balls === 2 ?
+     (<><div className="bg-orange-light rounded h-4 w-4 mx-2"></div>
+      <div className="bg-orange-light rounded h-4 w-4 mx-2"></div></>) 
+      :
+     (<><div className="bg-orange-light rounded h-4 w-4 mx-2"></div>
+      <div className="bg-orange-light rounded h-4 w-4 mx-2"></div> 
+      <div className="bg-orange-light rounded h-4 w-4 mx-2"></div></>) 
+      }
+    </div>
+    <div className="flex items-baseline my-3">
+      <h3 className="text-2xl">Outs: </h3>
+      {props.outs === 0 ? null : 
+      props.outs === 1 ? <div className="bg-red-light rounded h-4 w-4 mx-2"></div> :
+     (<><div className="bg-red-light rounded h-4 w-4 mx-2"></div>
+      <div className="bg-red-light rounded h-4 w-4 mx-2"></div></>)
+      }
+    </div>
     </div>
   )
 }
